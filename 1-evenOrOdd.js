dd385fb87cb851555bcd360302bd192c12870244
Yosua Suherman <suherman.yos@gmail.com>
2024-01-25 15:22:03 +0700

    Code Challenge Day 1/366

---
+++ 1-evenOrOdd.js
@@ -0,0 +1,22 @@
+// Coding Challenge 2024
+
+// 1/366
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//       return "Even";
+//     }
+//     else {
+//       return "Odd"
+//     }
+//   }
+
+// function evenOrOdd(number) {
+//   return number % 2 === 0 ? "Even" : "Odd";
+// }
+
+const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
+
+console.log(evenOrOdd(-48));
