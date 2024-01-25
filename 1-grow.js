dcfd6b25163a87e981f58d82b6a9baa58fc42df6
Yosua Suherman <suherman.yos@gmail.com>
2024-01-25 12:05:36 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,14 +1,22 @@
-function grow(x) {
-  let result = x[0];
-  for (let i = 1; i < x.length; i++) {
-    result *= x[i];
-  }
-  return result;
-}
+// WPU Coding Challenge 2024
+
+// 1/366
+
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
+// }
 
 // function grow(x) {
 //   const result = x.reduce((acc, curr) => acc * curr, 1);
 //   return result;
 // }
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
 
 console.log(grow([1, 2, 3, 4]));
---
+++ 1-numberToString.js
@@ -0,0 +1,6 @@
+function numberToString(num) {
+  const result = num.toString();
+  return result;
+}
+
+numberToString(67);
