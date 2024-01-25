9323ff44914475537a1a3882d2174b711f079c5c
Yosua Suherman <suherman.yos@gmail.com>
2024-01-25 10:16:36 +0700

    day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,14 @@
+function grow(x) {
+  let result = x[0];
+  for (let i = 1; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
+
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+console.log(grow([1, 2, 3, 4]));
