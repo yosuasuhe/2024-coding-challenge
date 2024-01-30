4aba5f362d3bec8ac92c08de6de472f68107cb58
Yosua Suherman <suherman.yos@gmail.com>
2024-01-30 07:49:33 +0700

    Day 5/366

---
+++ 5-zeroFuel.js
@@ -0,0 +1,11 @@
+// Coding Challenge 2024
+
+// 5/366
+
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump / mpg <= fuelLeft;
+};
+
+console.log(zeroFuel(100, 50, 1));
